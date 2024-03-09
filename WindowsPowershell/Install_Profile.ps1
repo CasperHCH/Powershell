1033fb17233fb8d5a654dda300893976fedb0329
Casper <casper-cj@hotmail.com>
2024-03-09 13:49:38 +0100

    Update Install_Profile.ps1

--- WindowsPowershell/Install_Profile.ps1
+++ WindowsPowershell/Install_Profile.ps1
@@ -1,3 +1,2 @@
-$destfolders = "C:\Program Files\PowerShell\7\profile.ps1", "C:\Users\caschr\Documents\PowerShell\profile.ps1", "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
-$destfolders | ForEach-Object{Copy-Item -path "./micosoft.Powershell_profile.ps1" -destination {Join-Path $_ $destFileName}}
-& $profile
+Copy-Item -path "C:\ps\WindowsPowershell\Microsoft.PowerShell_profile.ps1" -destination "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1" -Force
+. $profile
